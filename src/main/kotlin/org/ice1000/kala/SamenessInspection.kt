2c66dcf44a3504c6c71a7d6dd24a694fce92d77e
HoshinoTented <hoshinotented@qq.com>
2023-10-26 00:57:08 +0800

    inspection: better warning text

--- src/main/kotlin/org/ice1000/kala/SamenessInspection.kt
+++ src/main/kotlin/org/ice1000/kala/SamenessInspection.kt
@@ -1,6 +1,5 @@
 package org.ice1000.kala
 
-import com.intellij.codeInspection.CommonQuickFixBundle
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
@@ -8,33 +7,38 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.project.Project
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.util.InheritanceUtil
+import com.siyeh.InspectionGadgetsBundle
 
-// TODO: This also works for non-kala method
 class SamenessInspection : KalaInspection() {
   companion object {
     private const val METHOD_NAME: String = "sameElements"
   }
   
   private object FIX : LocalQuickFix {
-    override fun getFamilyName(): String = CommonQuickFixBundle.message("fix.replace.with.x", "true")
+    override fun getFamilyName(): String = InspectionGadgetsBundle.message("constant.conditional.expression.simplify.quickfix")
     
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
       descriptor.psiElement.replace(JavaPsiFacade.getElementFactory(project)
         .createExpressionFromText("true", descriptor.endElement))
     }
   }
-  
-  override fun getDisplayName(): String = KalaBundle.message("kala.sameness")
+
+  override fun getDisplayName(): String = InspectionGadgetsBundle.message("boolean.expression.can.be.simplified.problem.descriptor", "true")
   
   override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = methodCallVisitor {
     val args = it.argumentList.expressions
     val methodExpr = it.methodExpression
-    val self = methodExpr.qualifierExpression?.reference?.resolve() ?: return@methodCallVisitor
+    val selfExpr = methodExpr.qualifierExpression ?: return@methodCallVisitor
     if (args.isEmpty()) return@methodCallVisitor
     if (methodExpr.referenceName != METHOD_NAME) return@methodCallVisitor
-    val arg = args.first().reference?.resolve() ?: return@methodCallVisitor
-    
-    if (self == arg) {
+    val argExpr = args.first() ?: return@methodCallVisitor
+
+    val self = selfExpr.reference?.resolve() ?: return@methodCallVisitor
+    val arg = argExpr.reference?.resolve() ?: return@methodCallVisitor
+
+    if (InheritanceUtil.isInheritor(selfExpr.type, "$PKG.base.Traversable")
+      && self == arg) {
       holder.registerProblem(holder.manager.createProblemDescriptor(
         it, displayName, FIX, ProblemHighlightType.WARNING, isOnTheFly
       ))
