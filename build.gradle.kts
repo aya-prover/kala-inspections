ae900bac09875b73fb4367c4c877f063f7866167
ice1000 <ice1000kotlin@foxmail.com>
2025-02-03 13:10:13 -0500

    deps: An attempt to upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,30 +13,28 @@ version = properties("pluginVersion").get()
 // Configure project's dependencies
 repositories {
   mavenCentral()
+  intellijPlatform.defaultRepositories()
+}
+
+intellijPlatform.pluginConfiguration {
+  name = properties("pluginName")
 }
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
   testImplementation(libs.kala.collection)
+  intellijPlatform {
+    intellijIdeaCommunity("2024.3")
+    bundledPlugin("com.intellij.java")
+    testFramework(TestFrameworkType.Platform)
+  }
 }
 
 // Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
 kotlin {
-  @Suppress("UnstableApiUsage")
-  jvmToolchain {
-    languageVersion = JavaLanguageVersion.of(17)
-    vendor = JvmVendorSpec.JETBRAINS
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
   }
-}
-
-// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-  pluginName = properties("pluginName")
-  version = properties("platformVersion")
-  type = properties("platformType")
-
-  // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-  plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
 }
 
 tasks {
