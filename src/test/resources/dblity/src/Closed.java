019db2946b6d89e1ea4df8c9da24d1e76591849f
HoshinoTented <hoshinotented@qq.com>
2025-10-30 09:51:05 +0800

    misc: test

--- src/test/resources/dblity/src/Closed.java
+++ src/test/resources/dblity/src/Closed.java
@@ -1,6 +1,6 @@
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-@Target({ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE, ElementType.TYPE})
+@Target({ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE, ElementType.TYPE, ElementType.FIELD})
 public @interface Closed {
 }
